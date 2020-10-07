68ecb196c1e45f2f27618178efb07b80b4233cea
Saul Pizarroso <mortaldoom75@gmail.com>
2020-10-06 20:35:03 -0400

    base

---
+++ app/src/main/java/ucb/edu/bo/exam2/MainActivity.kt
@@ -0,0 +1,14 @@
+package ucb.edu.bo.exam2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar.let {
+            it?.setDisplayHomeAsUpEnabled(true)
+            it?.setDisplayShowHomeEnabled(true)}
+    }
+}
